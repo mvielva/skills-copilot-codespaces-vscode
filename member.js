8b142acfbd6358d4d86bc71e1926d75b5b0fc18a
Manuel Vielva <117858863+mvielva@users.noreply.github.com>
2023-08-10 11:08:39 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,4 +1,4 @@
-function skillMember() {
+function skillsMember() {
     var member = document.getElementById("member");
     var skill = document.getElementById("skill");
     member.style.display = "block";
