9c490915d7dd23bd92ce47a414e1c3e798a73fa8
Manuel Vielva <117858863+mvielva@users.noreply.github.com>
2023-08-10 11:22:14 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,38 @@
+//create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var fs = require('fs');
+var path = require('path');
+var port = 3000;
+
+//create web server
+var server = app.listen(port, function(){
+  console.log('Server running at http://localhost:' + port);
+});
+
+//get data from json file
+var comments = require('./comments.json');
+
+//get all comments
+app.get('/comments', function(req, res){
+  res.json(comments);
+});
+
+//get comment by id
+app.get('/comments/:id', function(req, res){
+  var id = req.params.id;
+  var comment = comments.filter(function(comment){
+    return comment.id == id;
+  });
+  res.json(comment[0]);
+});
+
+//add comment
+app.post('/comments', function(req, res){
+  var comment = {
+    id: req.body.id,}
+    comments.push(comment);
+    res.json(comment);
+});
